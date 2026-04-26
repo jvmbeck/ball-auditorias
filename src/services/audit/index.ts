e75941188392594ba1d2a7b6c1eb11f3de51efbb
João Vitor <jmadridbeck@gmail.com>
2026-04-26 19:44:21 -0300

    exportando novos arquivos

--- src/services/audit/index.ts
+++ src/services/audit/index.ts
@@ -3,3 +3,6 @@ export * from './auditResults';
 export * from './analytics.failuresOverTime';
 export * from './analytics.failuresByProcess';
 export * from './analytics.processFailureRate';
+export * from './createAuditService';
+export * from './checklistAuditService';
+export * from './boardAuditService';

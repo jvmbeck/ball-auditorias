f03a655bd384c1b83ee272aed70831ce281bf412
João Vitor <jmadridbeck@gmail.com>
2026-05-08 08:53:37 -0300

    removi os valores do .env pro github actions

--- src/boot/firebase.js
+++ src/boot/firebase.js
@@ -11,13 +11,13 @@ import { useAuthStore } from 'stores/auth.store';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  apiKey: 'AIzaSyAnmFT9TctviLXpaI6W3fPxvIDwFCE-qg4',
+  authDomain: 'ball-auditorias.firebaseapp.com',
+  projectId: 'ball-auditorias',
+  storageBucket: 'ball-auditorias.firebasestorage.app',
+  messagingSenderId: '515760332872',
+  appId: '1:515760332872:web:89be1e95ed92d03d47b87d',
+  measurementId: 'G-E6EQ15GKCS',
 };
 
 // Initialize Firebase
